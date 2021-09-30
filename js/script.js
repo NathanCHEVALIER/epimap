d32f8b0ca9f45e745419d94079f8ccade6c985cd
Matiboux <matiboux@gmail.com>
2021-10-01 01:28:33 +0200

    Refactor script.js

--- js/script.js
+++ js/script.js
@@ -1,39 +1,40 @@
-var btn = document.getElementById("btn-menu");
-var menu = document.getElementById("left-menu");
-var container = document.getElementById("container");
-var map_ifr = document.getElementById("map-ifr");
+const btn = document.getElementById("btn-menu");
+const menu = document.getElementById("left-menu");
+const container = document.getElementById("container");
+const map_ifr = document.getElementById("map-ifr");
 
 btn.classList.add("menu-open");
 menu.classList.add("menu-open");
 container.classList.add("menu-open");
 
 btn.addEventListener('click', function() {
-    if(btn.classList.contains("menu-open")){
+    if (btn.classList.contains("menu-open")) {
         btn.classList.remove("menu-open");
         menu.classList.remove("menu-open");
         container.classList.remove("menu-open");
     }
-    else{
+    else {
         btn.classList.add("menu-open");
         menu.classList.add("menu-open");
         container.classList.add("menu-open");
     }
 });
 
-var elements = document.querySelectorAll('#left-menu a');
-
-for ( var i = 0, len = elements.length; i < len; i++) {
+const elements = document.querySelectorAll('#left-menu a');
+const len = elements.length;
+for (let i = 0; i < len; ++i) {
     elements[i].addEventListener('click', function(e) {
+        e.preventDefault();
+
         var link = e.target.getAttribute("href");
-        if (link == "null" ){
-            e.preventDefault();
+        if (link === "null")
             return false;
-        }
-        if (link == "https://github.com/NathanCHEVALIER/epimap" ){
+
+        if (link === "https://github.com/NathanCHEVALIER/epimap") {
             window.open("https://github.com/NathanCHEVALIER/epimap", "_blank");
         }
-        else if (link == "" ){
-            alert("This map does not exist yet !")
+        else if (link === "") {
+            alert("This map does not exist yet !");
         }
         else {
             map_ifr.setAttribute("src", link);
@@ -42,7 +43,6 @@ for ( var i = 0, len = elements.length; i < len; i++) {
             container.classList.remove("menu-open");
         }
 
-        e.preventDefault();
         return false;
     });
 }
