47fc7beadbb99bd686e327842e3dda6320041a70
NathanCHEVALIER <nathanchevalier.lechatelet@gmail.com>
2021-12-13 23:44:55 +0100

    [DARKMODE] - Color Mode detection test

--- js/script.js
+++ js/script.js
@@ -282,3 +282,25 @@ const searchRender = function()
         container.append(dupBlock);
     }
 };
+
+
+
+/** Yet another kind of features to moove */
+
+const setColorMode = function()
+{
+    let matched = window.matchMedia('(prefers-color-scheme: dark)').matches;
+    
+    if (matched)
+	    console.log('Currently in dark mode');
+    else
+	    console.log('Currently in light mode');
+};
+
+window.matchMedia('(prefers-color-scheme: dark)').addEventListener('change', e => {
+    setColorMode();
+});
+
+DarkReader.disable();
+
+setColorMode();
