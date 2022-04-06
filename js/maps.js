ffa286acaacf8ef9f7c5892a1a63aa87dc9dd52b
NathanCHEVALIER <nathanchevalier.lechatelet@gmail.com>
2022-04-06 09:14:34 +0200

    [INTEGRATION]: Update script and files organization

--- js/maps.js
+++ js/maps.js
@@ -1,38 +1,3 @@
-const documentTitle = document.title;
-const container = document.getElementById("container");
-const mapnavname = document.querySelector("#map-nav .map-name");
-const mapnavdate = document.querySelector("#map-nav > div:nth-of-type(2) > span");
-
-
-const btn = document.getElementById("btn-menu");
-const menu = document.getElementById("left-menu");
-const cache = document.getElementById("cache");
-
-
-const httpRequest = function(url, type) {
-    return new Promise((resolve, reject) => {
-        let request = new XMLHttpRequest();
-        request.open('GET', url, true);
-        request.setRequestHeader('Content-Type', type);
-        request.onload = event => {
-            if (request.status < 200)
-                return reject("1xx Status: Partial Response: " + request.status);
-            else if (request.status == 304 || request.status == 200)
-                return resolve(request);
-            else if (request.status == 401)
-                return reject("401 Error: Bad Request, please contact dev team");
-            else if (request.status == 404)
-                return reject("404 Error: Resource not found");
-            else
-                return reject(request.status + " Error: Unexpected Error");
-        };
-        request.onerror = event => {
-            reject("Request Error: " + request.status);
-        };
-        request.send();
-    });
-};
-
 const loadMap = function(url) {
     httpRequest(url, 'image/svg+xml').then( function(body) {
         injectMap(body);
@@ -65,7 +30,20 @@ const onClickMapLink = function(e, path) {
     return false;
 };
 
-loadMap("/maps/kremlin-bicetre.svg");
+//loadMap("/maps/kremlin-bicetre.svg");
+
+const initMap = function()
+{
+    let path = window.location.href.split('/').pop();
+
+    if (path.length == 0)
+        path = "kremlin-bicetre";
+
+    loadMap("/maps/" + path + ".svg");
+    btn.classList.remove("menu-open");
+    menu.classList.remove("menu-open");
+    container.classList.remove("menu-open");
+};
 
 /*
 const mapIframeLoaded = function() {
@@ -85,16 +63,3 @@ const mapIframeLoaded = function() {
     );
 };
 */
-
-const initMap = function()
-{
-    let path = window.location.href.split('/').pop();
-
-    if (path.length == 0)
-        path = "kremlin-bicetre";
-
-    loadMap("/maps/" + path + ".svg");
-    btn.classList.remove("menu-open");
-    menu.classList.remove("menu-open");
-    container.classList.remove("menu-open");
-};
