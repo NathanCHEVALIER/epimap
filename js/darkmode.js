0fcce15c6f248bdf096d0b73ceffae8dcc380a16
NathanCHEVALIER <nathanchevalier.lechatelet@gmail.com>
2022-04-12 14:08:06 +0200

    [DARKMODE] - Add automatic color mode recognition and initialization

--- js/darkmode.js
+++ js/darkmode.js
@@ -1,42 +1,50 @@
-/** Yet another kind of features*/
+/** Color Mode */
 
-const map_ifr = document.getElementById("map-ifr");
-
-const getIframeDocument = function(ifr) {
-    const elt = (ifr.contentWindow || ifr.contentDocument);
-    return elt.document ? elt.document : elt;
-};
+const darkModeBtn = document.querySelector("#settings .onoffswitch:nth-of-type(1)");
 
 const getColorMode = function()
 {
     let matched = window.matchMedia('(prefers-color-scheme: dark)').matches;
     
     if (matched)
-	    console.log('Currently in dark mode');
+    {
+        console.log("System default currently in dark mode");
+        return true;
+    }
     else
-	    console.log('Currently in light mode');
-};
-
-window.matchMedia('(prefers-color-scheme: dark)').addEventListener('change', e => {
-    getColorMode();
-});
-
-// Check for Dark Reader plugin
-// DarkReader.disable();
-
-getColorMode();
-
-/** Colorize Maps */
-
-const ColorizeMap = function(svgDoc)
-{
-    var paths = svgDoc.querySelectorAll("path");
-
-    document.querySelector("body").classList.add("darkmode");
-
-    for (let i = 0; i < paths.length; i++) {
-        paths[i].setAttribute('style', 'stroke: #dddddd !important; fill: transparent;');
+    {
+        console.log("System default currently in light mode");
+        return false;
     }
 };
 
-ColorizeMap();
+window.matchMedia('(prefers-color-scheme: dark)').addEventListener('change', e => {
+    setColorMode(getColorMode());
+});
+
+const initColorMode = function() {
+    setColorMode(getColorMode());
+};
+
+darkmodeBtn.addEventListener('change', e => {
+    if (document.querySelector("body").classList.contains("darkmode"))
+        document.querySelector("body").classList.remove("darkmode");
+    else
+        document.querySelector("body").classList.add("darkmode");
+})
+
+const setColorMode = function(mode) {
+    const body = document.querySelector("body");
+    const colorModeSwitch = document.querySelector("#settings > div:nth-of-type(1) input[type=checkbox]");
+
+    colorModeSwitch.checked = mode;
+    body.className = "";
+
+    if (mode)
+        body.classList.add("darkmode");
+};
+
+initColorMode();
+
+// Check for Dark Reader plugin
+// DarkReader.disable();
