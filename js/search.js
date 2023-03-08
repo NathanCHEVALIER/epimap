1a4f29dc3e4e4aa7774d2865b216fba512006df2
NathanCHEVALIER <nathanchevalier.lechatelet@gmail.com>
2023-03-08 12:42:20 +0100

    Refactor Search

--- js/search.js
+++ js/search.js
@@ -1,112 +1,69 @@
-// DEPRECATED: should be updated
+const searchInput = document.querySelector('header > input[name="search"]');
 
 /*** Dev in progress */
 
-var results;
+let results;
 
 const search = function(str)
 {
     displayWarning("Search engine is deprecated. Please consider contributing!");
-    const nbmaps = Object.keys(maps).length;
     results = [];
     
     if (str === "")
         return;
 
-    maps.forEach(campus => {
-        campus.forEach(building => {
-            building.forEach(floor => {
-                
-            })
+    Object.keys(maps).forEach(campus => {
+        Object.keys(maps[campus].buildings).forEach(building => {
+            Object.keys(maps[campus].buildings[building].floors).forEach(floor => {
+                Object.keys(maps[campus].buildings[building].floors[floor].rooms).forEach(room => {
+                    const roomObj = maps[campus].buildings[building].floors[floor].rooms[room];
+    
+                    const subdist = gradeSet(roomObj.tags.concat(roomObj.name).concat(roomObj.peoples), str);
+                    if (subdist >= 0)
+                    {
+                        insertInPlace({
+                            key: roomObj.name,
+                            value: 0.1 * subdist,
+                            map: floor,
+                        });
+                    }
+            
+                });
+            });
         });
     });
-    
-        for (let i = /*nbmaps - 1*/17; i >= 0; --i)
-    {
-        searchTextInMap(Object.keys(maps)[i], str);
-    }
     
     setTimeout(searchRender, 100);
 };
 
-const searchTextInMap = function(map, str) {
-    let request = new XMLHttpRequest();
-    request.open("GET", 'maps/' + map + '.svg');
-    request.setRequestHeader("Content-Type", "image/svg+xml");
-    request.addEventListener("load", function(event) {
-        let response = event.target.responseText;
-        let doc = new DOMParser();
-        let svg = doc.parseFromString(response, "image/svg+xml");
-        const labels = svg.querySelectorAll('text > tspan');
+const gradeSet = (set, ref) => {
+    let grade = 0;
+    ref = ref.toLowerCase();
 
-        for (let i = 0; i < labels.length; ++i)
-        {
-            if (labels[i].textContent === "")
-                continue;
+    set.forEach( elt => {
+        elt = elt.toLowerCase();
 
-            const subdist = isSubString(labels[i].textContent, str);
-            if (subdist >= 0)
-            {
-                insertInPlace({
-                    key: labels[i].textContent,
-                    value: 0.1 * subdist,
-                    map: map,
-                });
-                continue;
-            }
-            
-            /*const d = editDist(labels[i].textContent, str, labels[i].textContent.length, str.length);
-            if (labels[i].textContent.length > 0 && d <= (0.4 * str.length))
-            {
-                insertInPlace({
-                    key: labels[i].textContent,
-                    value: 0.3 * str.length,
-                    map: map
-                });
-            }*/
-        }
+        if (elt.includes(ref))
+            grade += 1;
     });
-    request.send();
-};
 
-const editDist = function(str1, str2, m, n)
-{
-    if (m == 0)
-        return n;
- 
-    if (n == 0)
-        return m;
- 
-    if (str1[m - 1] == str2[n - 1])
-        return editDist(str1, str2, m - 1, n - 1);
- 
-    return 1 + Math.min(
-        editDist(str1, str2, m, n - 1),
-        editDist(str1, str2, m - 1, n),
-        editDist(str1, str2, m - 1, n - 1));
-};
+    return grade == 0 ? -1 : grade;
+}
 
-const isSubString = function(str1, str2)
-{
-    const len = str2.length;
-    str1 = str1.toUpperCase();
-    str2 = str2.toUpperCase();
+const isSubString = (str1, str2) => {
+    str1 = str1.toLowerCase();
+    str2 = str2.toLowerCase();
 
-    for (let i = 0; i <= str1.length - len; i++)
-    {
-        if (str1.substring(i, i + len) === str2)
-            return str1.length - len;
+    if (str1.includes(str2)) {
+        return str1.length - str2.length;
     }
 
     return -1;
 };
 
-const insertInPlace = function(obj)
-{
-    for (let i = 0; i < results.length; i++)
-    {
-        if (obj['value'] < results[i]['value'])
-        {
+const insertInPlace = (obj) => {
+    for (let i = 0; i < results.length; i++) {
+        if (obj['value'] < results[i]['value']) {
             results.splice(i, 0, obj);
             return;
         }
@@ -116,25 +73,26 @@ const insertInPlace = function(obj)
 };
 
 searchBtn.addEventListener('click', function() {
-    search(document.querySelector('header > input[name="search"]').value);
+    search(searchInput.value);
 });
 
-document.querySelector('header > input[name="search"]').addEventListener("keydown", function(e) {
-    if (e.keyCode === 13 && 
-        document.querySelector('header > input[name="search"]') === document.activeElement) 
-    {
+searchInput.addEventListener("keydown", function(e) {
+    if (e.keyCode === 13 && searchInput === document.activeElement) {
         e.preventDefault();
-        search(document.querySelector('header > input[name="search"]').value);
+        search(searchInput.value);
     }
 });
 
 const searchRender = function()
 {
+    infoMenu.classList.add("menu-open");
+    infoMenu.querySelector('div:nth-of-type(1)').style.display = 'none';
+    document.getElementById("btn-menu").classList.add("menu-back");
+
     console.log(results);
 
-    /*
-    const container = searchTemplate.parentNode;
 
+    const container = searchTemplate.parentNode;
     while (container.lastChild !== searchTemplate) {
         container.removeChild(container.lastChild);
     }
@@ -144,20 +102,21 @@ const searchRender = function()
         let dupBlock = searchTemplate.cloneNode([true]);
 
         dupBlock.children[0].innerHTML = results[i]['key'];
-        dupBlock.children[1].innerHTML = map_dname = maps[results[i]['map']]['d_name'];
+        dupBlock.children[1].innerHTML = 'Lol';//aps[results[i]['map']]['d_name'];
         dupBlock.removeAttribute('id');
         dupBlock.setAttribute("href", results[i]['map'])
         dupBlock.style.display = "block";
 
         dupBlock.addEventListener('click', function() {
+            alert('not comptible for the moment');
             //map_ifr.setAttribute("src", "./maps/" + results[i]['map'] + ".svg");
-            loadMap("/maps/" + results[i]['map'] + ".svg");
+            /*loadMap("/maps/" + results[i]['map'] + ".svg");
             btn.classList.remove("menu-open");
             menu.classList.remove("menu-open");
             container.classList.remove("menu-open");
-            cache.classList.remove("menu-open");
+            cache.classList.remove("menu-open");*/
         });
 
         container.append(dupBlock);
-    }*/
+    }
 };
